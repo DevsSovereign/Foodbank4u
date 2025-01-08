9bcb0b13dc0a4a6ccbe9f8a2ccf9fb7c6accbe76
Wineshuga <uzochukwuwinnie@gmail.com>
2025-01-08 22:15:22 +0100

    Add header responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,58 @@
+import { useState } from "react";
+import { RiCloseLargeFill } from "react-icons/ri";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from "react-router-dom";
+
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <nav className="m-2 py-3 pl-3 pt-3 md:pl-5 md:py-7 flex items-center justify-between">
-    <span className="flex items-center gap-2">
-      <img src="/assets/foodbank-logo.png" alt="foodbank logo" />
-      <p className="font-bold text-secondary text-xl sm:block hidden">
-        FOODBANK
-      </p>
-    </span>
-  </nav>
+    <nav className="flex items-center justify-between py-4 md:px-14 px-5 relative z-30">
+      <span className="flex items-center gap-2">
+        <img
+          src="/assets/foodbank-logo.png"
+          className="sm:w-auto w-10"
+          alt="foodbank logo"
+        />
+        <p className="font-bold text-secondary text-xl">FOODBANK</p>
+      </span>
+      <button type="button" onClick={() => setShowMenu(true)}>
+        <RxHamburgerMenu className="w-7 h-7 sm:hidden" />
+      </button>
+      <ul
+        className={`gap-4 sm:flex ${
+          showMenu
+            ? "bg-secondary flex-col p-5 pb-10 flex text-white absolute z-30 top-0 right-0 sm:w-1/2 w-full md:hidden"
+            : "hidden items-center "
+        }`}
+      >
+        {showMenu && (
+          <button type="button" onClick={() => setShowMenu(false)}>
+            <RiCloseLargeFill className="w-7 h-7 sm:hidden" />
+          </button>
+        )}
+        <li>
+          <Link to={""}>Our Process</Link>
+        </li>
+        <li>
+          <Link to={""}>FAQs</Link>
+        </li>
+        <li>
+          <Link to={""}>Partner With Us</Link>
+        </li>
+        <li className="md:ml-10 mx-auto">
+          <button
+            type="button"
+            className={`border-2 py-2 rounded-full md:px-10 ${
+              showMenu ? "px-10" : "border-secondary text-secondary px-4"
+            }`}
+          >
+            Get Started
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
