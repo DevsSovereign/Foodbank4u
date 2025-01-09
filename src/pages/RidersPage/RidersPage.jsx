76772d6f23b6f4e714e4e8c1f09d20da01bc7fe5
Wineshuga <uzochukwuwinnie@gmail.com>
2025-01-09 21:59:21 +0100

    Use all riders page components

---
+++ src/pages/RidersPage/RidersPage.jsx
@@ -0,0 +1,45 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import ExclusiveSection from "../LandingPage/ExclusiveSection";
+import FaqSection from "../LandingPage/FaqSection";
+import HeroSection from "./HeroSection";
+import PartnershipSection from "./PartnershipSection";
+import faqData from "./faqData.json";
+import benefitsData from "./benefits.json";
+
+const RidersPage = () => {
+  return (
+    <section className="max-w-[1450px] mx-auto overflow-hidden">
+      <Header
+        customStyle={"absolute w-full text-white"}
+        textColor={"text-white"}
+      />
+      <HeroSection />
+      <ExclusiveSection
+        data={benefitsData}
+        mainText={"Benefits of Partnering with Us"}
+        subText1={"Rider’s Benefit"}
+        subText2={" with Foodbank"}
+        details={
+          "Join Foodbank's rider network and be part of a community that makes a difference! As a rider, you'll play a vital role in delivering food assistance to our beneficiaries."
+        }
+      />
+      <PartnershipSection />
+      <FaqSection data={faqData} />
+      <section className="flex flex-col md:flex-row justify-between items-center py-10 px-10">
+        <h3 className="md:text-4xl text-2xl md:w-1/2 text-center mb-5 md:text-left md:mb-0 md:leading-[3.5rem] font-bold">
+          Partner With Foodbank Today and feed the Nation
+        </h3>
+        <button
+          type="button"
+          className="w-40 bg-secondary text-white p-3 rounded-md"
+        >
+          Apply Now
+        </button>
+      </section>
+      <Footer />
+    </section>
+  );
+};
+
+export default RidersPage;
