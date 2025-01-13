5e0b60c0c6c81b0f93cbdfd9d933b11c0cde2db9
Wineshuga <uzochukwuwinnie@gmail.com>
2025-01-13 02:04:12 +0100

    Apply carousel to testimonial section

--- src/components/landingPage/ReviewCard.jsx
+++ src/components/landingPage/ReviewCard.jsx
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 
 const ReviewCard = ({ text, name }) => {
   return (
-    <section className="bg-black relative p-10 w-[37rem] pl-24 mx-3 rounded-lg text-white font-inter">
-      <span className="absolute left-5">
+    <section className="bg-black relative p-8 md:pt-14 lg:pt-10 lg:h-64 md:h-80 sm:h-60 cursor-pointer pl-16 md:pl-10 lg:pl-24 mx-3 rounded-lg text-white font-inter">
+      <span className="absolute left-3 md:top-3 lg:left-5 lg:top-10">
         <img src="/assets/icons/quotes.png" alt="" />
       </span>
       <p>{text}</p>
