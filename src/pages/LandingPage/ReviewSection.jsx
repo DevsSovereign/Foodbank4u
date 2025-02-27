8bcd07deb91fe2491ea2ad35e4d0b6cc493248f8
Wineshuga <uzochukwuwinnie@gmail.com>
2025-02-27 22:48:46 +0100

    Use testimony endpoint

--- src/pages/LandingPage/ReviewSection.jsx
+++ src/pages/LandingPage/ReviewSection.jsx
@@ -1,10 +1,12 @@
 import ReviewCard from "../../components/landingPage/ReviewCard";
-import testimonyData from "./testimony.json";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const ReviewSection = () => {
+  const [testimonies, setTestimonies] = useState([]);
   const settings = {
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -29,6 +31,20 @@ const ReviewSection = () => {
     ],
   };
 
+  useEffect(() => {
+    const getTestimonies = async () => {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_API_URL}/testimonies`
+        );
+        setTestimonies(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getTestimonies();
+  }, []);
+
   return (
     <section className="py-20">
       <h3 className="text-center text-secondary font-ppins">TESTIMONIALS</h3>
@@ -36,13 +52,19 @@ const ReviewSection = () => {
         HONEST REVIEWS AND SUCCESS STORIES
       </p>
       <section className="py-10 ">
-      <Slider {...settings}>
-        {testimonyData.map((testimony, index) => (
-          <section key={index}>
-            <ReviewCard text={testimony.text} name={testimony.name} />
-          </section>
-        ))}
+        {testimonies && testimonies.length > 0 ? (
+          <Slider {...settings}>
+            {testimonies.map((testimony, index) => (
+              <section key={index}>
+                <ReviewCard text={testimony.content} name={testimony.name} />
+              </section>
+            ))}
           </Slider>
+        ) : (
+          <div className="border w-2/3 mx-auto h-40 flex justify-center items-center shadow-md">
+            <p className="text-3xl font-semibold text-center">Testimonies</p>
+          </div>
+        )}
       </section>
     </section>
   );
