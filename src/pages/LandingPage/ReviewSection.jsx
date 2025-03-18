e964d013c46365350c7ada8e70fde53394c38756
Wineshuga <uzochukwuwinnie@gmail.com>
2025-03-18 21:26:56 +0100

    Fix testimony slider

--- src/pages/LandingPage/ReviewSection.jsx
+++ src/pages/LandingPage/ReviewSection.jsx
@@ -4,6 +4,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import data from "./testimony.json";
 
 const ReviewSection = () => {
   const [testimonies, setTestimonies] = useState([]);
@@ -59,7 +60,7 @@ const ReviewSection = () => {
         HONEST REVIEWS AND SUCCESS STORIES
       </p>
       <section className="py-10 ">
-        {testimonies && testimonies.length > 0 ? (
+        {testimonies && testimonies.length > 2 ? (
           <Slider {...settings}>
             {testimonies.map((testimony, index) => (
               <section key={index}>
@@ -67,6 +68,18 @@ const ReviewSection = () => {
               </section>
             ))}
           </Slider>
+        ) : testimonies && testimonies.length > 0 && testimonies.length < 3 ? (
+          <section
+            className={`grid grid-cols-${testimonies.length} w-2/3 mx-auto gap-4`}
+          >
+            {testimonies.map((testimony, index) => (
+              <ReviewCard
+                key={index}
+                text={testimony.content}
+                name={testimony.name}
+              />
+            ))}
+          </section>
         ) : (
           <div className="border w-2/3 mx-auto h-40 flex justify-center items-center shadow-md">
             <p className="text-3xl font-semibold text-center">Testimonies</p>
